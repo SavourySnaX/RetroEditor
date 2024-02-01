8965145762c42c64e63896ac27a0e95622d99100
Savoury SnaX <savoury.snax@googlemail.com>
2024-02-01 14:43:29 +0000

    Add back mastersystem

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-var romPlugins = new IRomPlugin[] { new ZXSpectrum(), new Megadrive(), new NintendEntertainmentSystem() };
+var romPlugins = new IRomPlugin[] { new ZXSpectrum(), new Megadrive(), new NintendEntertainmentSystem(), new MasterSystem() };
 var plugins = new IRetroPlugin[] { new JetSetWilly48(), new Rollercoaster(), new Fairlight(), new PhantasyStar2(), new Metroid() };
 
 var render = new Editor(plugins,romPlugins);
