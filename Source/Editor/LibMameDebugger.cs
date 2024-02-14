18de7f2f3db07b9e62d7f40a6aea477bb6a95600
Savoury SnaX <savoury.snax@googlemail.com>
2024-02-14 16:38:29 +0000

    Add support for multiple views (free view currently crashes)

--- Source/Editor/LibMameDebugger.cs
+++ Source/Editor/LibMameDebugger.cs
@@ -6,13 +6,14 @@ internal class LibMameDebugger
 {
     public class DView
     {
-        public DView(int viewNum, int x, int y, int w, int h)
+        public DView(LibRetroPlugin.RetroDebugView view, int x, int y, int w, int h, string expression)
         {
-            this.viewNum = viewNum;
-            this.x = x;
-            this.y = y;
-            this.w = w;
-            this.h = h;
+            this.view = view;
+            this.view.X=x;
+            this.view.Y=y;
+            this.view.W=w;
+            this.view.H=h;
+            this.view.Expression=expression;
             this.state = new byte[w * h * 2];
             for (int i = 0; i < w * h * 2; i += 2)
             {
@@ -21,21 +22,27 @@ internal class LibMameDebugger
             }
         }
 
-        public int viewNum;
-        public int x, y, w, h;
+        public LibRetroPlugin.RetroDebugView view;
+
         public byte[] state;
     }
 
 
     private LibRetroPlugin plugin;
 
-    private LibRetroPlugin.DebuggerView debuggerViewCallback;
+    private LibRetroPlugin.DebuggerView debuggerViewCallbacks;
     private LibRetroPlugin.RemoteCommand remoteCommandCallback;
 
 
+    private int stateViewCounter;
+    private int disasmCounter;
+    private int memoryCounter;
 
     public LibMameDebugger(LibRetroPlugin plugin)
     {
+        this.stateViewCounter=0;
+        this.disasmCounter=0;
+        this.memoryCounter=0;
         this.plugin = plugin;
         plugin.SetDebuggerCallback(DebuggerCallback);
     }
@@ -55,11 +62,15 @@ internal class LibMameDebugger
     {
         if (ImGui.MenuItem("CPU State"))
         {
-            editor.OpenWindow(new DebuggerView(this, 1, 20, 25), "CPU State");
+            editor.OpenWindow(new DebuggerView(this, LibRetroPlugin.debug_view_type.State, 20, 25, ""), $"CPU State {stateViewCounter++}");
         }
         if (ImGui.MenuItem("Disassembly"))
         {
-            editor.OpenWindow(new DebuggerView(this, 0, 100, 25), "Disassembly");
+            editor.OpenWindow(new DebuggerView(this, LibRetroPlugin.debug_view_type.Disassembly, 100, 25, "curpc"), $"Disassembly {disasmCounter++}");
+        }
+        if (ImGui.MenuItem("Memory"))
+        {
+            editor.OpenWindow(new DebuggerView(this, LibRetroPlugin.debug_view_type.Memory, 20, 25, "0"), $"Memory {memoryCounter++}");
         }
         if (ImGui.MenuItem("Console"))
         {
@@ -67,18 +78,44 @@ internal class LibMameDebugger
         }
     }
 
-    public void UpdateDView(ref DView view)
+    public LibRetroPlugin.RetroDebugView AllocView(LibRetroPlugin.debug_view_type kind)
     {
-        int viewSize = view.w * view.h * 2;
-        if (debuggerViewCallback.viewCb == null)
+        if (debuggerViewCallbacks.allocCb == null)
+        {
+            return new LibRetroPlugin.RetroDebugView();
+        }
+        unsafe
+        {
+            var _this = (void*)debuggerViewCallbacks.data;
+            var view = debuggerViewCallbacks.allocCb(_this, kind);
+            return new LibRetroPlugin.RetroDebugView(view);
+        }
+    }
+
+    public void FreeView(LibRetroPlugin.RetroDebugView view)
+    {
+        if (debuggerViewCallbacks.freeCb == null)
         {
             return;
         }
-        var exp = Marshal.StringToHGlobalAnsi("cur_pc");
         unsafe
         {
-            var _this = (void*)debuggerViewCallback.data;
-            var view_char = debuggerViewCallback.viewCb(_this, view.x, view.y, view.w, view.h, view.viewNum, (byte*)exp);
+            var _this = (void*)debuggerViewCallbacks.data;
+            debuggerViewCallbacks.freeCb(_this, view.view);
+        }
+    }
+
+    public void UpdateDView(ref DView view)
+    {
+        int viewSize = view.view.W * view.view.H * 2;
+        if (debuggerViewCallbacks.viewCb == null)
+        {
+            return;
+        }
+        unsafe
+        {
+            var _this = (void*)debuggerViewCallbacks.data;
+            var view_char = debuggerViewCallbacks.viewCb(_this, view.view.view);
             Marshal.Copy((nint)view_char,view.state,0,viewSize);
         }
     }
@@ -104,7 +141,7 @@ internal class LibMameDebugger
         switch (kind)
         {
             case 0:
-                debuggerViewCallback = Marshal.PtrToStructure<LibRetroPlugin.DebuggerView>(data);
+                debuggerViewCallbacks = Marshal.PtrToStructure<LibRetroPlugin.DebuggerView>(data);
                 return 1;
             case 1:
                 remoteCommandCallback = Marshal.PtrToStructure<LibRetroPlugin.RemoteCommand>(data);
