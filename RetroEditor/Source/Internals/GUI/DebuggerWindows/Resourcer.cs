51438b863dd173252dac17d672953ab84ef102f2
Savoury SnaX <savoury.snax@googlemail.com>
2025-03-26 15:41:20 +0000

    safe

--- RetroEditor/Source/Internals/GUI/DebuggerWindows/Resourcer.cs
+++ RetroEditor/Source/Internals/GUI/DebuggerWindows/Resourcer.cs
@@ -1,5 +1,7 @@
 using ImGuiNET;
+using System.Diagnostics;
 using System.Numerics;
+using System.Text;
 
 /*
 
@@ -29,15 +31,14 @@ internal class Resourcer : IWindow
     public float UpdateInterval => 1.0f;
     
     LibMameDebugger debugger;
+    RomDataParser romData;
     string traceFile = "trace.txt";
     List<string> traceLog;
     bool traceInProgress = false;
+    bool romLoaded = false;
 
     // Memory map data
     private const int MEMORY_MAP_HEIGHT = 50;
-    private const uint ROM_SIZE = 512*1024; 
-    private HashSet<uint> codeAddresses = new HashSet<uint>();
-    private HashSet<uint> dataAddresses = new HashSet<uint>();
     private Vector4 unknownColor = new Vector4(0, 0, 0, 1); // Black
     private Vector4 codeColor = new Vector4(0, 0, 1, 1);    // Blue
     private Vector4 dataColor = new Vector4(1, 0, 0, 1);    // Red
@@ -151,10 +152,7 @@ internal class Resourcer : IWindow
 
         // Update memory map data
         // Add all addresses from this instruction to code addresses
-        for (uint i = 0; i < instruction.Size; i++)
-        {
-            codeAddresses.Add(address + i);
-        }
+        romData.AddCodeRange(address, (uint)(address + instruction.Size));
 
         // Check operands for data references
         foreach (var operand in instruction.Operands)
@@ -168,7 +166,7 @@ internal class Resourcer : IWindow
                     var addrStr = operand.Text.TrimStart('$', '(', ')', '[', ']');
                     if (uint.TryParse(addrStr, System.Globalization.NumberStyles.HexNumber, null, out uint dataAddr))
                     {
-                        dataAddresses.Add(dataAddr);
+                        romData.AddDataRange(dataAddr, dataAddr);   // TODO - need to add the size of the data reference
                     }
                 }
             }
@@ -355,6 +353,8 @@ internal class Resourcer : IWindow
     {
         this.debugger = debugger;
         traceLog = new List<string>();
+
+        var parser = new RomDataParser();
     }
 
     public void Close()
@@ -368,28 +368,32 @@ internal class Resourcer : IWindow
         var size = ImGui.GetContentRegionAvail();
         size.Y = MEMORY_MAP_HEIGHT;
 
+
+        // Compute the size of the memory map
+        var minAddress = romData.GetMinAddress;
+        var maxAddress = romData.GetMaxAddress;
+        var range = maxAddress - minAddress;
+        var scale = size.X / range;
+
         // Draw background (unknown regions)
         drawList.AddRectFilled(pos, new Vector2(pos.X + size.X, pos.Y + size.Y), ImGui.GetColorU32(unknownColor));
 
-        // Draw code regions
-        foreach (var addr in codeAddresses)
+        foreach (var region in romData.GetRomRanges)
         {
-            float x = pos.X + (addr * size.X / ROM_SIZE);
+            var color = unknownColor;
+            switch (region.Value.Region)
+            {
+                case Regions.Code:
+                    color = codeColor;
+                    break;
+                case Regions.Data:
+                    color = dataColor;
+                    break;
+            }
             drawList.AddRectFilled(
-                new Vector2(x, pos.Y),
-                new Vector2(x + 1, pos.Y + size.Y),
-                ImGui.GetColorU32(codeColor)
-            );
-        }
-
-        // Draw data regions
-        foreach (var addr in dataAddresses)
-        {
-            float x = pos.X + (addr * size.X / ROM_SIZE);
-            drawList.AddRectFilled(
-                new Vector2(x, pos.Y),
-                new Vector2(x + 1, pos.Y + size.Y),
-                ImGui.GetColorU32(dataColor)
+                new Vector2(pos.X + region.Start * scale, pos.Y),
+                new Vector2(pos.X + region.End * scale, pos.Y + size.Y),
+                ImGui.GetColorU32(color)
             );
         }
 
@@ -405,7 +409,7 @@ internal class Resourcer : IWindow
         {
             ImGui.BeginDisabled();
         }
-        if (ImGui.Button("Start Capture"))
+        if (ImGui.Button("Capture Frame"))
         {
             traceInProgress = true;
             // Set up trace logging
@@ -413,12 +417,266 @@ internal class Resourcer : IWindow
             // Wait for vblank
             debugger.SendCommand("gvblank");
         }
+        if (ImGui.Button("Capture 100 Milliseconds"))
+        {
+            traceInProgress = true;
+            // Set up trace logging
+            debugger.SendCommand($"trace {traceFile},,noloop");
+            // Wait for vblank
+            debugger.SendCommand("gtime 100");
+        }
         if (traceDisable)
         {
             ImGui.EndDisabled();
         }
 
+//        DisplayRomData();       
+        ScrollableTableView();
 
+        return false;
+    }
+
+    internal unsafe class ImGuiClipper : IDisposable
+    {
+        public ImGuiClipper(int itemsCount, float itemsHeight)
+        {
+            _itemsCount = itemsCount;
+            _itemsHeight = itemsHeight;
+            _clipper = ImGuiNative.ImGuiListClipper_ImGuiListClipper();
+        }
+
+        public void Begin()
+        {
+            ImGuiNative.ImGuiListClipper_Begin(_clipper, _itemsCount, _itemsHeight);
+        }
+
+        public bool Step()
+        {
+            return ImGuiNative.ImGuiListClipper_Step(_clipper) != 0;
+        }
+
+        public void End()
+        {
+            ImGuiNative.ImGuiListClipper_End(_clipper);
+        }
+
+        public void Dispose()
+        {
+            ImGuiNative.ImGuiListClipper_destroy(_clipper);
+        }
+
+        public int DisplayStart => _clipper->DisplayStart;
+        public int DisplayEnd => _clipper->DisplayEnd;
+
+        private int _itemsCount;
+        private float _itemsHeight;
+        private ImGuiListClipper* _clipper;
+    }
+
+    private void ScrollableTableView()
+    {
+        // Start Table and render header
+        float availableHeight = ImGui.GetContentRegionAvail().Y;
+        float tableHeight = availableHeight;
+        var lineHeight = ImGui.GetTextLineHeight() + ImGui.GetStyle().ItemSpacing.Y;
+
+        ImGuiTableFlags tableFlags = ImGuiTableFlags.Borders |
+                                    ImGuiTableFlags.Resizable;
+        if (ImGui.BeginTable("RomDataHeader", 3, tableFlags, new Vector2(0, lineHeight)))
+        {
+            // Set up columns
+            ImGui.TableSetupColumn("Address", ImGuiTableColumnFlags.WidthFixed, 100);
+            ImGui.TableSetupColumn($"Bytes", ImGuiTableColumnFlags.WidthFixed, 30 * 16);
+            ImGui.TableSetupColumn("ASCII", ImGuiTableColumnFlags.WidthStretch);
+
+            // Header row
+            ImGui.TableHeadersRow();
+
+            ImGui.EndTable();
+        }
+
+        var contentSize = ImGui.GetContentRegionAvail();
+        if (ImGui.BeginChild("Virtual Table", contentSize, ImGuiChildFlags.None, ImGuiWindowFlags.AlwaysVerticalScrollbar))
+        {
+            var scroll = ImGui.GetScrollY();
+
+            var rom = romData.GetRomData;
+            var romStartOffset = romData.GetMinAddress;
+            var romEndOffset = romData.GetMaxAddress;
+
+            var regions = romData.GetRomRanges;
+            var romDataSize = rom.Length;
+
+            foreach (var region in regions)
+            {
+                Console.WriteLine($"Region: {region.Start:X8} - {region.End:X8} : {region.Value.LineCount}");
+            }
+
+            var address = 0 + scroll/lineHeight;
+            if (ImGui.BeginTable("RomDataVirt", 3, tableFlags, new Vector2(0, tableHeight)))
+            {
+                ImGui.TableSetupColumn("Address", ImGuiTableColumnFlags.WidthFixed, 100);
+                ImGui.TableSetupColumn($"Bytes", ImGuiTableColumnFlags.WidthFixed, 30 * 16);
+                ImGui.TableSetupColumn("ASCII", ImGuiTableColumnFlags.WidthStretch);
+
+                using var clipper = new ImGuiClipper((int)romDataSize, lineHeight);
+                clipper.Begin();
+                while (clipper.Step())
+                {
+                    UInt64 curAddress = (UInt64)address;
+                    for (int i = clipper.DisplayStart; i < clipper.DisplayEnd; i++)
+                    {
+                        ImGui.TableNextRow();
+                        var fetched = regions.GetRangeContainingPoint(curAddress);
+
+                        if (fetched==null)
+                        {
+                            Console.WriteLine($"WTF");
+                            break;
+                        }
+                        if (fetched.Value.Region == Regions.Code)
+                        {
+                            ImGui.TableSetBgColor(ImGuiTableBgTarget.RowBg0, ImGui.GetColorU32(codeColor));
+                        }
+                        else if (fetched.Value.Region == Regions.Data)
+                        {
+                            ImGui.TableSetBgColor(ImGuiTableBgTarget.RowBg0, ImGui.GetColorU32(dataColor));
+                        }
+                        else
+                        {
+                            ImGui.TableSetBgColor(ImGuiTableBgTarget.RowBg0, ImGui.GetColorU32(unknownColor));
+                        }
+                        ImGui.TableNextColumn();
+                        ImGui.Text($"{curAddress:X8}");
+                        ImGui.TableNextColumn();
+                        var s = new StringBuilder();
+                        if (fetched != null)
+                        {
+                            byte value = romData.GetByte(curAddress);
+                            s.Append($"{value:X2} ");
+                        }
+                        else
+                        {
+                            s.Append("   ");
+                        }
+                        ImGui.Text(s.ToString());
+                        ImGui.TableNextColumn();
+                        s.Clear();
+                        if (fetched != null)
+                        {
+                            byte value = romData.GetByte(curAddress);
+                            s.Append(char.IsControl((char)value) ? '.' : (char)value);
+                        }
+                        else
+                        {
+                            s.Append(' ');
+                        }
+                        curAddress++;
+                        ImGui.TextUnformatted(s.ToString());
+                    }
+                }
+                clipper.End();
+                ImGui.EndTable();
+            }
+            ImGui.EndChild();
+        }
+    }
+
+    private void DisplayRomData()
+    {
+        var rom = romData.GetRomData;
+        var romDataSize = rom.Length;
+        var romStartOffset = romData.GetMinAddress;
+        var romEndOffset = romData.GetMaxAddress;
+
+        var regions = romData.GetRomRanges;
+
+        // Calculate total bytes and lines needed
+        var totalBytes = romEndOffset - romStartOffset;
+        var bytesPerLine = 16;
+        var totalLines = (totalBytes + (UInt64)bytesPerLine - 1) / (UInt64)bytesPerLine;
+
+        // Calculate visible lines based on window height
+        var lineHeight = ImGui.GetTextLineHeight() + ImGui.GetStyle().ItemSpacing.Y;
+        var visibleLines = (int)(ImGui.GetContentRegionAvail().Y / lineHeight);
+        var scrollMax = Math.Max(0, (int)totalLines - visibleLines);
+
+        // Add vertical scrollbar
+        ImGui.BeginChild("RomDataTable", new Vector2(0, 0), ImGuiChildFlags.None, ImGuiWindowFlags.HorizontalScrollbar);
+        
+        // Table flags for resizable columns and borders
+        ImGuiTableFlags tableFlags = ImGuiTableFlags.Borders | 
+                                   ImGuiTableFlags.Resizable |
+                                   ImGuiTableFlags.ScrollY;
+
+
+        float totalHeight = (float)totalLines * lineHeight;
+        float availableHeight = ImGui.GetContentRegionAvail().Y;
+        float tableHeight = Math.Min(totalHeight, availableHeight);
+
+        if (ImGui.BeginTable("RomData", 17, tableFlags, new Vector2(0, tableHeight)))
+        {
+            // Set up columns
+            ImGui.TableSetupColumn("Address", ImGuiTableColumnFlags.WidthFixed, 100);
+            for (int i = 0; i < 16; i++)
+            {
+                ImGui.TableSetupColumn($"B{i:X1}", ImGuiTableColumnFlags.WidthFixed, 30);
+            }
+            ImGui.TableSetupColumn("ASCII", ImGuiTableColumnFlags.WidthStretch);
+
+            // Header row
+            ImGui.TableHeadersRow();
+
+            // Display ROM data
+            for (UInt64 line = 0; line < totalLines; line++)
+            {
+                ImGui.TableNextRow();
+                UInt64 lineU64 = (UInt64)line;
+                UInt64 bytesPerLineU64 = (UInt64)bytesPerLine;
+                UInt64 product = lineU64 * bytesPerLineU64;
+                UInt64 lineAddress = romStartOffset + product;
+
+                // Address column
+                ImGui.TableNextColumn();
+                ImGui.Text($"{lineAddress:X8}");
+
+                // Bytes columns
+                StringBuilder asciiBuilder = new StringBuilder();
+                for (int i = 0; i < 16; i++)
+                {
+                    ImGui.TableNextColumn();
+                    UInt64 currentAddress = lineAddress + (UInt64)i;
+                    if (currentAddress <= romEndOffset)
+                    {
+                        byte value = romData.GetByte(currentAddress);
+                        ImGui.Text($"{value:X2}");
+                        asciiBuilder.Append(char.IsControl((char)value) ? '.' : (char)value);
+                    }
+                    else
+                    {
+                        ImGui.Text("  ");
+                        asciiBuilder.Append(' ');
+                    }
+                }
+
+                // ASCII column
+                ImGui.TableNextColumn();
+                ImGui.Text(asciiBuilder.ToString());
+
+                if (ImGui.GetContentRegionAvail().Y < 0)
+                {
+                    break;
+                }
+            }
+
+            ImGui.EndTable();
+        }
+
+        ImGui.EndChild();
+    }
+
+    private void DisplayTrace()
+    {
         // Display trace log
         if (ImGui.BeginChild("TraceLog", new System.Numerics.Vector2(0, 0), ImGuiChildFlags.None, ImGuiWindowFlags.HorizontalScrollbar))
         {
@@ -498,8 +756,6 @@ internal class Resourcer : IWindow
             ImGui.PopFont();
             ImGui.EndChild();
         }
-
-        return false;
     }
 
     public bool Initialise()
@@ -509,7 +765,17 @@ internal class Resourcer : IWindow
 
     public void Update(float seconds)
     {
-        // TODO
+        if (!romLoaded)
+        {
+            // Read ROM data in chunks
+            romData = new RomDataParser();
+
+            romData.Parse(debugger);
+            
+            romLoaded = true;
+        }
+
+        // Handle trace in progress
         if (traceInProgress)
         {
             if (debugger.IsStopped)
@@ -526,11 +792,35 @@ internal class Resourcer : IWindow
                     disassembly.Clear();
                     foreach (var line in traceLog)
                     {
-                        ParseDisassembly(line);
+                        //ParseDisassembly(line);
+                        ParseLocation(line);
+                        break;
                     }
                 }
                 traceInProgress = false;
             }
         }
     }
+
+    private void ParseLocation(string line)
+    {
+        // Format: BANK:OFFSET mnemonic operands
+        var parts = line.Split(' ');
+        if (parts.Length < 2) return;
+
+        var addressParts = parts[0].Split(':');
+        if (addressParts.Length < 2) return;
+
+        if (!uint.TryParse(addressParts[0], System.Globalization.NumberStyles.HexNumber, null, out uint bank) ||
+            !uint.TryParse(addressParts[1], System.Globalization.NumberStyles.HexNumber, null, out uint offset))
+            return;
+
+        uint address = (bank << 16) | offset;
+
+        // Update memory map data
+        // Add all addresses from this instruction to code addresses
+        romData.AddCodeRange(address, address);
+    }
+
+
 }
